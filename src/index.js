afcab73aa5e80f41a24155d9d285d44274ef0851
Vlad <repovlad@gmail.com>
2023-08-18 00:06:31 +0300

    solution.v1

--- src/index.js
+++ src/index.js
@@ -1,23 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  for (let i = 0; i < str.length; i++) {
-    let currentSymbol = str[i];
-
-    if (OPEN_BRACKETS.includes(currentSymbol)) {
-        bracketsConfig.push(currentSymbol);
-    } else {
-        if (bracketsConfig.length === 0) {
-            return false;
+  function check(str, bracketsConfig) {
+    const stack = [];
+    for (const char of str) {
+      if (bracketsConfig[0].includes(char)) {
+        stack.push(char);
+      } else if (bracketsConfig[1].includes(char)) {
+        if (stack.length === 0 || bracketsConfig[0][stack.pop()] !== char) {
+          return false;
         }
-
-        let topElement = bracketsConfig[bracketsConfig.length - 1];
-        
-        if(BRACKETS_PAIR[currentSymbol] === topElement) {
-            bracketsConfig.pop();
-        } else {
-            return false;
-        }
+      }
     }
-}
-
-return bracketsConfig.length === 0;
+    return stack.length === 0;
+  }
 }
