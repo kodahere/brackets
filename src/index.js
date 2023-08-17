2711127ebe23e5186ac2f70fb12e8baa302a5dc7
Vlad <repovlad@gmail.com>
2023-08-18 00:01:20 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for (let i = 0; i < str.length; i++) {
+    let currentSymbol = str[i];
+
+    if (OPEN_BRACKETS.includes(currentSymbol)) {
+        bracketsConfig.push(currentSymbol);
+    } else {
+        if (bracketsConfig.length === 0) {
+            return false;
+        }
+
+        let topElement = bracketsConfig[bracketsConfig.length - 1];
+        
+        if(BRACKETS_PAIR[currentSymbol] === topElement) {
+            bracketsConfig.pop();
+        } else {
+            return false;
+        }
+    }
+}
+
+return bracketsConfig.length === 0;
 }
